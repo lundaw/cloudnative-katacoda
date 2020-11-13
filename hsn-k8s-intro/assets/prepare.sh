fd335087f11038a43e5e113c3c3180ffb8d45301
Kovács Tamás <kt@famulus.hu>
2020-11-13 16:47:56 +0100

    Try to fix coloring

--- hsn-k8s-intro/assets/prepare.sh
+++ hsn-k8s-intro/assets/prepare.sh
@@ -1,21 +1,21 @@
 # Add colors to bashrc
 printf "
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHTGRAY='\033[0;37m'
-DARKGRAY='\033[1;30m'
-LIGHTRED='\033[1;31m'
-LIGHTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-LIGHTBLUE='\033[1;34m'
-LIGHTPURPLE='\033[1;35m'
-LIGHTCYAN='\033[1;36m'
-WHITE='\033[1;37m'" >> ~/.bashrc
+NOCOLOR='\\e[0m'
+RED='\\e[0;31m'
+GREEN='\\e[0;32m'
+ORANGE='\\e[0;33m'
+BLUE='\\e[0;34m'
+PURPLE='\\e[0;35m'
+CYAN='\\e[0;36m'
+LIGHTGRAY='\\e[0;37m'
+DARKGRAY='\\e[1;30m'
+LIGHTRED='\\e[1;31m'
+LIGHTGREEN='\\e[1;32m'
+YELLOW='\\e[1;33m'
+LIGHTBLUE='\\e[1;34m'
+LIGHTPURPLE='\\e[1;35m'
+LIGHTCYAN='\\e[1;36m'
+WHITE='\\e[1;37m'" >> ~/.bashrc
 source ~/.bashrc
 
 # Add package repo, install recommended dependencies, set up environment
@@ -24,37 +24,37 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-echo "${YELLOW}Updating apt package list${NOCOLOR}"
+ech0 -e "${YELLOW}Updating apt package list${NOCOLOR}"
 apt -qq update
 
 # Install recommended dependencies and k8s
-echo "${YELLOW}Installing k8s packages and dependencies${NOCOLOR}"
+ech0 -e "${YELLOW}Installing k8s packages and dependencies${NOCOLOR}"
 apt -q install -y --no-install-recommends apt-transport-https ebtables kubelet kubeadm kubectl
 
 # Run kubeadm to set up cluster
-echo "${YELLOW}Pulling k8s images and configuring cluster${NOCOLOR}"
+ech0 -e "${YELLOW}Pulling k8s images and configuring cluster${NOCOLOR}"
 kubeadm config images pull
 kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # Install kubectl tab completion
-echo "${YELLOW}Configuring kubectl autocompletion${NOCOLOR}"
+ech0 -e "${YELLOW}Configuring kubectl autocompletion${NOCOLOR}"
 mkdir -p /etc/bash_completion.d
 bash -c 'source /etc/bash_completion'
 bash -c 'echo "source <(kubectl completion bash)" > /etc/bash_completion.d/kubectl'
 
 # Set up kube config
-echo "${YELLOW}Copying kube config${NOCOLOR}"
+ech0 -e "${YELLOW}Copying kube config${NOCOLOR}"
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Remove control-plane node isolation and make scheduling possible on the control-plane node
-echo "${YELLOW}Removing isolation taint from every node${NOCOLOR}"
+ech0 -e "${YELLOW}Removing isolation taint from every node${NOCOLOR}"
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # Set up networking
-echo "${YELLOW}Setting up networking for k8s${NOCOLOR}"
+ech0 -e "${YELLOW}Setting up networking for k8s${NOCOLOR}"
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 # Signal being done
-echo "${GREEN}Environment is ready to use!${NOCOLOR}"
+ech0 -e "${GREEN}Environment is ready to use!${NOCOLOR}"
